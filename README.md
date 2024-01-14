b2ce83879a80b0f39a8eeaf7c302b6316dc47449
luckinha <luckinha45@gmail.com>
2024-01-14 19:37:05 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Midpoint Webhook
+
+Um simples projeto para facilitar o uso de **Webhooks** com os sistemas da CommercePlus e HoopDecor. A ideia dele é deixar uma máquina local acessível para o uso de Webhooks usando [Ngrok](https://ngrok.com/), e utilizar um app em [Node.js](https://nodejs.org/en) para redirecionar as requests para o projeto principal que processa os Webhooks recebidos.
--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-var bodyParser = require('body-parser');
 const axios = require('axios');
 const { response } = require('express');
 const app = express();
@@ -9,12 +8,8 @@ const port = 3000;
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
+//#region CommercePlus
 
-// https://8a44-2804-818c-408c-6c00-2605-e000-8ded-8967.ngrok-free.app/PostPed/968E874B-F378-48EE-B02D-E3E4D0ADE192
-// HTTP://SISTEMALIFT1.COM:8082/API/INTEGRACAOLIFT/PostPedidoCommPlus/968E874B-F378-48EE-B02D-E3E4D0ADE192
-
-// WEBHOOK COMMERCEPLUS
-// key color: 968E874B-F378-48EE-B02D-E3E4D0ADE192
 app.post('/PostPed/:apikey', async (req, res) => {
 	console.log(req.body);
 	const key = req.params.apikey;
@@ -34,18 +29,16 @@ app.post('/PostPed/:apikey', async (req, res) => {
 		res.send(error);
 	});
 })
+//#endregion
 
-// https://9fcb-2804-818c-408c-6c00-2605-e000-8ded-8967.ngrok-free.app/PostOrc/E1E2B1BA-A4B8-42A5-9E19-E4633BF86F9C
+//#region HoopDecor
 
-// WEBHOOK HOOP
-// key lift: E1E2B1BA-A4B8-42A5-9E19-E4633BF86F9C
-// key DE: F3A9F930-39D1-48AC-9DC3-774D1084040F
 app.post('/PostOrc/:apikey', async (req, res) => {
 	console.log(req.body);
 	const key = req.params.apikey;
 
 	// Enviando para o Visual Studio
-	axios.post(`http://localhost:56464/IntegracaoLift/PostOrcamento/${key}`, {
+	axios.post(`http://localhost:13523/IntegracaoLift/PostOrcamento/${key}`, {
 			data: req.body.data,
 		})
 		.then((r) => {
@@ -53,7 +46,7 @@ app.post('/PostOrc/:apikey', async (req, res) => {
 			res.send(r.data);
 		})
 		.catch((error) => {
-			//console.log(error);
+			console.log(error);
 			res.send(error);
 		});
 })
@@ -61,3 +54,4 @@ app.post('/PostOrc/:apikey', async (req, res) => {
 app.listen(port, () => {
 	console.log(`Server started at http://localhost:${port}\n`)
 })
+//#endregion
