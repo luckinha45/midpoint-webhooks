310f6c951880f7e1840394d0327ac9add262c4b5
luckinha <luckinha45@gmail.com>
2024-11-13 10:12:38 -0300

    add nuvemshop

--- app.js
+++ app.js
@@ -1,12 +1,61 @@
 const express = require('express');
 const axios = require('axios');
 const { response } = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
 // muda a forma de interpretar o dado enviado, se for um json, usa a primeira, se for no formato de de form de post, usa a segunda
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
+
+//#region NuvemShop
+app.post('/PostPedNuvemShop/:apikey', async (req, res) => {
+	console.log(req.body);
+	const key = req.params.apikey;
+
+	// Enviando para o Visual Studio
+	axios.post(`http://localhost:13523/IntegracaoLift/PostPedNuvemShop/${key}`, {
+		data: req.body.data,
+	})
+	.then((r) => {
+		//console.log(r.data);
+		res.send(r.data);
+	})
+	.catch((error) => {
+		console.log(error);
+		res.send(error);
+	});
+})
+//#endregion
+
+//#region VTEX
+
+app.post('/PostPedidoVTEX/:apikey', async (req, res) => {
+	console.log(req.body);
+	const key = req.params.apikey;
+
+	// checando se é apenas um ping
+	if (req.body?.hookConfig == 'ping') {
+		res.send('ok');
+		return;
+	}
+
+	// Enviando para o Visual Studio
+
+	//data: req.body,
+	axios.post(`http://localhost:13523/IntegracaoLift/PostPedidoVTEX/${key}`, {...req.body})
+	.then((r) => {
+		console.log(r.data);
+		res.send(r.data);
+	})
+	.catch((error) => {
+		console.log(error);
+		res.send(error);
+	});
+})
+//#endregion
+
 
 //#region CommercePlus
 
@@ -16,7 +65,7 @@ app.post('/PostPed/:apikey', async (req, res) => {
 
 	// Enviando para o Visual Studio
 	
-	axios.post(`http://localhost:56464/IntegracaoLift/PostPedidoCommPlus/${key}`, {
+	axios.post(`http://localhost:13523/IntegracaoLift/PostPedidoCommPlus/${key}`, {
 		//data: req.body,
 		...req.body
 	})
